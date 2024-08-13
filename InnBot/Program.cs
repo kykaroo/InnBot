9f7fd03ddb0bea722e5936c44272e005b2cafced
kykaroo <boris_nemcev@mail.ru>
2024-08-13 20:08:57 +0500

    Добавлен конфиг для ключа бота

---
+++ InnBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("secrets.json", true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Build();
